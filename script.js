bc6047a56eea084cc767e50c2f0c1b4fdcbd4142
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-10-20 17:48:33 -0400

    added css, html and js to mak eit look alive and functional

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const emojis = ["🐶","🐶","🐱","🐱","🦊","🦊","🐻","🐻","🐥","🐥","🦅","🦅","🐸","🐸","🦕","🦕"];
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+for ( var i =0; i<emojis.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+
+    box.onclick = function(){
+        this.classList.add('boxOpen')
+        setTimeout(function(){
+            if(document.querySelectorAll('.boxOpen').length > 1){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+                    document.querySelectorAll('.boxOpen')[0].classList.add
+                    ('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add
+                    ('box.Match')
+                }
+            }
+        },500)
+    }
+    document.querySelector('.game').appendChild(box);
+}
